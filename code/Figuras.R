fa13bb4d140fba6e00ff8ede32ec0390dd500821
Angelini <angelini75@gmail.com>
2019-06-28 09:59:50 -0300

    se separó los paneles de la figura 1a, angulo 45deg x, figura a el doble de alta que figura b

--- code/Figuras.R
+++ code/Figuras.R
@@ -25,7 +25,10 @@ d <- st_as_sf(d)
 arg <- st_as_sf(arg)
 
 maps <- ggplot() + geom_sf(data = arg) + geom_sf(data = d, size = 0.3, aes(color = type)) +
-  facet_wrap(facets = "type") + theme_grey() + labs(x = "Long", y = "Lat") + theme(legend.position="none")
+  facet_wrap(facets = "type") + theme_grey() + labs(x = "Long", y = "Lat") + 
+  theme(legend.position="none") +
+  theme(text=element_text(size=12,  family="serif"), 
+        axis.text.x = element_text(angle = 45)) + theme(panel.spacing = unit(2, "lines"))
 
 d <- as_tibble(d)
 d$Date <- d$Date %>% as.character() %>% strptime("%Y-%m-%d") %>% as.POSIXct()
@@ -33,8 +36,10 @@ d$Year <- as.numeric(substring(d$Date,1,4))
 
 hist <- ggplot(data = d, aes(Year, fill = type)) + geom_histogram(binwidth = 1) + 
   scale_x_continuous(breaks = c(1955, 1960,1970, 1980,1990,2000, 2005,2010,2015)) + theme_grey() + 
-  + theme(legend.position="top")
+ theme(legend.position="top") +  scale_fill_discrete("") + labs(x = "Years", y = "Count") +
+  theme(text=element_text(size=12,  family="serif")) 
 
-fig1 <- plot_grid(maps, hist, labels = c("a", "b"), nrow = 2, align = "v")
+fig1 <- plot_grid(maps, hist, labels = c("a", "b"), nrow = 2, align = "v",
+                  label_fontfamily = "serif", rel_heights = c(2, 1))
 
-ggsave(filename = "fig1.png", plot = fig1, width = 5, height = 7)
+ggsave(filename = "results/fig1.png", plot = fig1, width = 5, height = 7)
